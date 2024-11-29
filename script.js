e55ad7eb9b7d4458e160aa003194a51ec78c8c76
ANJ-NISH <155297183+ANJ-NISH@users.noreply.github.com>
2024-11-29 18:04:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+
+let f1=document.getElementById("form1");
+
+f1.addEventListener("submit",
+				   function(e)
+					{
+						e.preventDefault();
+						let fn=document.getElementById("fn").value;
+						let ln=document.getElementById("ln").value;
+						let pn=document.getElementById("pn").value;
+						let ei=document.getElementById("ei").value;
+
+						alert(`First Name: ${fn} Last Name: ${ln} Phone Number: ${pn} Email ID: ${ei}`);
+					})
